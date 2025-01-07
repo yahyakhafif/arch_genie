85ac00bfa881f26391fa84861e1aa644248c7921
yahyaAirbus <yahya.khafif@airbus.com>
2025-01-07 15:25:02 +0200

    front end setup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to react front end</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ server/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template 
+
+app = Flask(__name__, template_folder="../client/template")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+    pass
+
+@app.route('/login')
+def login():
+    pass
+
+@app.route('/signup')
+def signup():
+    pass
+
+@app.route('/designs')
+def designs():
+    pass
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="4000")
+    
