96ba34b442ea3c23c70f7d0849f586592c89a138
yahyaAirbus <yahya.khafif@icloud.com>
2025-01-13 12:46:27 +0200

    background image and main page done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import mainPage from './components/mainPage';
+import MainPage from './components/MainPage'
 
 function App() {
   return (
     <div>
-      <mainPage />
+      <MainPage />
     </div>
   );
 }
--- client/src/components/mainPage.js
+++ client/src/components/mainPage.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import '../App.css'
 
-const mainPage = () => {
+const MainPage = () => {
     return (
-        <div className="index-page">
-            <h1>Architecture Genie</h1>
-            <p>Your gateway to architectural wonders and artistic inspiration.</p>
-            <img src="/background-image.jpg" alt="Architectural landscape" />
-
-            <div className="cta-buttons">
-                <Link to="/login" className="cta-button">Sign In</Link>
-                <Link to="/signup" className="cta-button">Create Account</Link>
+        <Router>
+            <div className="index-page">
+                <h1>Architecture Genie</h1>
+                <p>Your gateway to architectural wonders and artistic inspiration.</p>
+                <div className="cta-buttons">
+                    <Link to="/login" className="cta-button">Sign In</Link>
+                    <Link to="/signup" className="cta-button">Create Account</Link>
+                </div>
             </div>
-        </div>
+        </Router>
     );
 };
 
-export default mainPage;
+export default MainPage;
